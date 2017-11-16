a5aacee278cef8a953dcead3e019a102072abe5c
Jon Roma <roma@illinois.edu>
2017-11-16 17:35:46 -0600

    Woof.

--- lambda/foo.py
+++ lambda/foo.py
@@ -2,6 +2,11 @@ from __future__ import print_function
 
 import json
  
+from datetime import datetime
+
+#####
+
 def handler(event, context):
+    print(datetime.now())
     print(json.dumps(event, indent=4))
     return
